c1d1222d91949743d199c44f17fdd180af966736
d2nst <wlsdl950@gmail.com>
2022-07-20 21:27:00 +0900

    folex시작

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  // Main Page Fullpage
+  $('#fullpage').fullpage({});
+
+  // Main Page Slide
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});

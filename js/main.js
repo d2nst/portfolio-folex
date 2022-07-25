d6d29dde8007c2cce9188d012c5b556a791565a2
d2nst <wlsdl950@gmail.com>
2022-07-25 22:36:05 +0900

    첫번째 페이지 완성

--- js/main.js
+++ js/main.js
@@ -1,6 +1,71 @@
 $(document).ready(function () {
-  // Main Page Fullpage
-  $('#fullpage').fullpage({});
+  if ($('#fullpage').length > 0) {
+    $('#fullpage').fullpage({
+      //options here
+      autoScrolling: true,
+      css3: true,
+      anchors: [
+        'firstPage',
+        'secondPage',
+        'thirdPage',
+        'forthPage',
+        'fifthPage',
+      ],
+      easing: 'easeInOutCubic',
+      easingcss3: 'ease-out',
+      afterLoad: function (origin, destination, direction, trigger) {
+        // 색인 사용
+
+        if (destination > 1) {
+          $('.fixed-nav').fadeIn();
+          $('.header nav').addClass('active');
+          $('.logo img').attr('src', './images/logo-dark.png');
+          $('.toggle-btn img').attr('src', './images/menu_icon-dark.png');
+        } else {
+          // $('.fixed-nav').fadeOut();
+          $('.header nav').removeClass('active');
+          $('.logo img').attr('src', './images/logo.png');
+          $('.toggle-btn img').attr('src', './images/menu_icon.png');
+        }
+
+        if (destination == 6) {
+          $('.back_to_top').addClass('active');
+        } else {
+          $('.back_to_top').removeClass('active');
+        }
+
+        if (destination == 2 || destination == 4) {
+          $('.fixed-nav').find('a').removeClass('active');
+          $('.fixed-nav').find('a').removeClass('active-black');
+          $('.fixed-nav')
+            .find('a')
+            .eq(destination - 1)
+            .addClass('active-black');
+        } else {
+          $('.fixed-nav').find('a').removeClass('active');
+          $('.fixed-nav').find('a').removeClass('active-black');
+          $('.fixed-nav')
+            .find('a')
+            .eq(destination - 1)
+            .addClass('active');
+        }
+        // if (destination === 6) {
+        //   $('.back_to_top').addClass('active');
+        //   $('.fixed-nav').fadeOut();
+        // } else {
+        //   $('.back_to_top').removeClass('active');
+        // }
+
+        // $('.fixed-nav').find('a').removeClass('active');
+        // $('.fixed-nav')
+        //   .find('a')
+        //   .eq(destination - 1)
+        //   .addClass('active');
+      },
+    });
+    //methods
+    $.fn.fullpage.setAllowScrolling(true);
+  }
 
   // Main Page Slide
   const swiper = new Swiper('.swiper', {
